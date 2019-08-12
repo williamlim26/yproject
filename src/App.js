448965f2bca3122d3a5cd3de4ca5dce102b10a9a
William <weinianlim26@gmail.com>
2019-08-12 13:19:55 -0700

    add yervana component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div style={{textAlign: "center"}}>
+        <h1 style={{padding: 5, border: "2px solid black", display: "inline-block", borderRadius: 5}}>yervana</h1>
+      </div>
+    </main>
   );
 }
 
